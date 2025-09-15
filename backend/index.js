2f1071d02ef03cd7c628fb40e047a1e6ac984d94
Aarav Vaish <aaravvaish2004@gmail.com>
2025-09-15 15:27:55 +0530

    env file config

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,25 @@
-import expess from 'express';
+import express from 'express';
 import cors from 'cors';
-const app = expess();
-const PORT = 3000;
+import 'dotenv/config'; 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+  origin: process.env.FRONTEND_URL
 }));
 
-app.get('/words', async (req, res)=>{
-    try{
-const fetchdata = await fetch('https://api.frontendexpert.io/api/fe/wordle-words');
-const data = await fetchdata.json();
-res.json(data);
-
-    }
-    catch(e){
-        console.log("Error -> ", e);
-    }
-
+app.get('/words', async (req, res) => {
+  try {
+    const fetchdata = await fetch(process.env.API_URL);
+    const data = await fetchdata.json();
+    res.json(data);
+  } catch (e) {
+    console.error("Error ->", e);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 });
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
